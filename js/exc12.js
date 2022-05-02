22779c8e86a3669cda10cce2ba9f39d68505b767
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-05-02 10:48:58 -0300

    operacoes com letras e numeros

--- js/exc12.js
+++ js/exc12.js
@@ -1,1 +1,2 @@
-document.write("Eu acertei: "+ ((15/50) *100));
+document.write(" exc 12 - Eu acertei: "+ ((15/50) *100));
+document.write("<br>")
---
+++ js/exc13.js
@@ -0,0 +1,2 @@
+document.write(" exc13 - A" + "B" + 20 + 10 + "C" + (5 + 10) + "D");
+document.write("<br>")
