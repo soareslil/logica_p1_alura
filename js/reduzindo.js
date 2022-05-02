e6c9fd5bb83ec2e285faec076078b99e5cbdebe0
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-05-02 10:56:59 -0300

    reduzindo alteracoes, utilizando variaveis

---
+++ js/reduzindo.js
@@ -0,0 +1,8 @@
+let ano = 2016;
+
+document.write("exc 14 - flavio tem" + (ano - 1997) + " anos")
+document.write("<br>")
+
+ano = 2017
+document.write("exc 14 - flavio tem" + (ano - 1997) + " anos")
+document.write("<br>")
