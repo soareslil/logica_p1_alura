fd2446f550b86b54d61e224c587b0f3e4041ffac
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-05-03 12:26:57 -0300

    erro de escrita

---
+++ js/exc18.js
@@ -0,0 +1,9 @@
+let parcelas = [10.40, 40.00, 100.18, 200];
+console.log("aqui1", parcelas)
+
+for(let i = 0; i < parcelas.length; i++) {
+    document.write(parcelas[i]);
+    console.log("aqui2", parcelas)
+}
+document.write("FIM");
+console.log("aqui3", parcelas)
